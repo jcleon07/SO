3393b44f19cd97d6d64ddb3a5c1ac8e59e23ee2e
Juan López <jcleon0722@gmail.com>
2025-09-16 22:54:14 -0500

    Add files via upload

---
+++ C and C++/pipe/calculo_pi_con_pipe.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int pipefd[2];
+    int r;
+    pid_t pid;
+    int iteraciones = 10000000000;
+    double pi = 0.0;
+    double pi_rec;
+
+    r = pipe(pipefd);
+    if (r < 0) {
+        perror("error pipe:");
+        exit(1);
+    }
+
+    pid = fork();
+  
+
+    if (pid == 0) { 
+        //Proceso hijo
+        close(pipefd[0]);
+        for (int i = 0; i < iteraciones; i++){
+            pi += 4.0/(4.0*i+1.0);
+        } 
+        write(pipefd[1],&pi, sizeof(double));
+        close(pipefd[1]); 
+        exit(0);
+    } else { 
+        //Proceso padre
+        close(pipefd[1]); 
+        for (int i = 1; i < iteraciones; i++){
+            pi -= 4.0/(4.0*i-1.0);
+        } 
+        read(pipefd[0], &pi_rec, sizeof(double));
+        pi += pi_rec;
+        printf("pi: %.10f\n", pi);
+        close(pipefd[0]);
+    }
+
+    return 0;
+}
